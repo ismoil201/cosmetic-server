2b20705531eeeeb7d85a673e9cf88f5d868b2c3a
ismoil <ismoiljoraxonov1@gmail.com>
2025-12-15 12:25:56 +0900

    edit cartItem Product

--- src/main/java/com/example/backend/entity/CartItem.java
+++ src/main/java/com/example/backend/entity/CartItem.java
@@ -2,21 +2,24 @@ package com.example.backend.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Data
 @Table(name = "cart_items")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties({"password"})
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER) // 🔥 MUHIM
     private Product product;
-
 
     private int quantity;
 }
+
