98950485fe62ea1016992a3287eaf6173cd490e0
ismoil <ismoiljoraxonov1@gmail.com>
2026-02-27 03:20:04 +0900

    add firebase-admin sdk

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("com.google.firebase:firebase-admin:9.4.3")
+
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
