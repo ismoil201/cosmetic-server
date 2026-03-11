614088bc073d1514e4adc75bb48e01b38def77fa
ismoil <ismoiljoraxonov1@gmail.com>
2026-03-12 01:32:48 +0900

    added77

--- src/main/java/com/example/backend/dto/OrderCreateRequest.java
+++ src/main/java/com/example/backend/dto/OrderCreateRequest.java
@@ -1,12 +1,34 @@
 package com.example.backend.dto;
 
-import lombok.Data;
 import java.util.List;
 
-@Data
 public class OrderCreateRequest {
 
     private Long receiverId;
     private Long addressId;
     private List<Long> cartItemIds;
+
+    public Long getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Long receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public Long getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Long addressId) {
+        this.addressId = addressId;
+    }
+
+    public List<Long> getCartItemIds() {
+        return cartItemIds;
+    }
+
+    public void setCartItemIds(List<Long> cartItemIds) {
+        this.cartItemIds = cartItemIds;
+    }
 }
