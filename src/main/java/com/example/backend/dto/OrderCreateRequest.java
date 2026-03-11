1f9ffe11a04f39a1671ab9ca768989329d17721b
ismoil <ismoiljoraxonov1@gmail.com>
2026-03-12 01:28:34 +0900

    added

--- src/main/java/com/example/backend/dto/OrderCreateRequest.java
+++ src/main/java/com/example/backend/dto/OrderCreateRequest.java
@@ -1,11 +1,12 @@
 package com.example.backend.dto;
 
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class OrderCreateRequest {
 
     private Long receiverId;
     private Long addressId;
+    private List<Long> cartItemIds;
 }
-
